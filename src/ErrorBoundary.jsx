a44ee5906ae1c82bd02fb86dc619d46dd53c8642
Marcus <marcustyphoon@gmail.com>
2022-09-02 01:47:02 -0700

    Add error boundary

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+
+export default function ErrorBoundary({ children, resetKeys }) {
+  const fallbackRender = React.useCallback(function FallbackComponent({ error }) {
+    return (
+      <div>
+        Rendering error caught! Please report this on{' '}
+        <a
+          href="https://github.com/marcustyphoon/gw2-rotation-formatter"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{' '}
+        or Discord.
+        <pre>{error.message}</pre>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  }, []);
+  return (
+    <ReactErrorBoundary fallbackRender={fallbackRender} resetKeys={resetKeys}>
+      {children}
+    </ReactErrorBoundary>
+  );
+}
