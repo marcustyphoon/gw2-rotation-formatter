3c6b60b5585aa3a2807840e458bd32c31cd9d4de
Marcus <marcustyphoon@gmail.com>
2022-09-07 12:06:04 -0700

    extract RotationSkill

--- src/RotationDisplay.jsx
+++ src/RotationDisplay.jsx
@@ -4,6 +4,50 @@ import { Fragment } from 'react';
 import { WEAPON_SWAP } from './constants';
 import { cancelledSkill, container, row } from './RotationDisplay.css';
 
+function RotationSkill({ id, cancelled, count, data, splitAutoChains, showInstantsAsInstant }) {
+  const { idsSet, isSwap, autoAttack, instant } = data ?? {
+    idsSet: new Set([id]),
+    isSwap: false,
+    autoAttack: false,
+    instant: false,
+  };
+  let content = '';
+
+  const className = cancelled ? cancelledSkill : false;
+
+  if (isSwap || id === WEAPON_SWAP) {
+    content = <div style={{ fontSize: autoAttack ? '0.7em' : '1em' }}>-</div>;
+  } else if (instant && showInstantsAsInstant) {
+    content = (skillId) => (
+      <div style={{ width: 0 }}>
+        <div
+          style={{
+            position: 'relative',
+            top: '20px',
+            fontSize: autoAttack ? '0.7em' : '1em',
+          }}
+        >
+          <Skill id={skillId} disableText />
+        </div>
+      </div>
+    );
+  } else {
+    content = (skillId) => (
+      <div style={{ fontSize: autoAttack ? '0.7em' : '1em' }}>
+        <Skill id={skillId} disableText className={className} />
+      </div>
+    );
+  }
+
+  const ids = [...idsSet];
+
+  return Array(count)
+    .fill()
+    .map((_, i) => (
+      <Fragment key={i}>{content(splitAutoChains ? ids[i % ids.length] : id)}</Fragment>
+    ));
+}
+
 export default function RotationDisplay({
   rotation,
   splitAutoChains = true,
@@ -14,52 +58,13 @@ export default function RotationDisplay({
       {rotation.map(({ skillSequence, label }, rowIndex) => (
         <div className={row} key={rowIndex}>
           {label}:
-          {skillSequence.map(({ id, cancelled, count, data }, skillIndex) => {
-            const { idsSet, isSwap, autoAttack, instant } = data ?? {
-              idsSet: new Set([id]),
-              isSwap: false,
-              autoAttack: false,
-              instant: false,
-            };
-            let content = '';
-            // const cancelledStyle = cancelled ?  : {};
-
-            const className = cancelled ? cancelledSkill : false;
-
-            if (isSwap || id === WEAPON_SWAP) {
-              content = <div style={{ fontSize: autoAttack ? '0.7em' : '1em' }}>-</div>;
-            } else if (instant && showInstantsAsInstant) {
-              content = (skillId) => (
-                <div style={{ width: 0 }}>
-                  <div
-                    style={{
-                      position: 'relative',
-                      top: '20px',
-                      fontSize: autoAttack ? '0.7em' : '1em',
-                    }}
-                  >
-                    <Skill id={skillId} disableText />
-                  </div>
-                </div>
-              );
-            } else {
-              content = (skillId) => (
-                <div style={{ fontSize: autoAttack ? '0.7em' : '1em' }}>
-                  <Skill id={skillId} disableText className={className} />
-                </div>
-              );
-            }
-
-            const ids = [...idsSet];
-
-            return Array(count)
-              .fill()
-              .map((_, innerSkillIndex) => (
-                <Fragment key={`${skillIndex}-${innerSkillIndex}`}>
-                  {content(splitAutoChains ? ids[innerSkillIndex % ids.length] : id)}
-                </Fragment>
-              ));
-          })}
+          {skillSequence.map(({ id, cancelled, count, data }, i) => (
+            <RotationSkill
+              {...{ id, cancelled, count, data }}
+              {...{ splitAutoChains, showInstantsAsInstant }}
+              key={i}
+            />
+          ))}
         </div>
       ))}
     </div>
